3d7e97f8c5af40ba360e047aa703bb425f782355
Jenna Chinn <chinnjenna@gmail.com>
2025-02-07 18:02:25 -0800

    improved hashtag selection

--- backend/public/scripts/generateCheckboxes.js
+++ backend/public/scripts/generateCheckboxes.js
@@ -15,6 +15,7 @@ function createCheckboxes(category, categoryOptions, containerId) {
     checkbox.type = "checkbox";
     checkbox.name = category;
     checkbox.value = key; // Set the value to the key
+    checkbox.id = key;
 
     const checkboxText = document.createElement("span");
     checkboxText.className = "checkbox-text";
@@ -26,23 +27,5 @@ function createCheckboxes(category, categoryOptions, containerId) {
   }
 }
 category.then((data) => {
-  console.log(data);
   for (let i = 0; i < data.length; ++i) createCheckboxes(String(i),data[i],String(i));
 });
-
-/*
-createCheckboxes("personality", category.personality, "personality-options");
-createCheckboxes("mbti", category.mbti, "mbti-options");
-createCheckboxes(
-  "entertainment",
-  category.entertainment,
-  "entertainment-options"
-);
-createCheckboxes("game", category.game, "game-options");
-createCheckboxes("food", category["food&beverage"], "food-options");
-createCheckboxes("drink", category["drink&party"], "drink-options");
-createCheckboxes("sports", category["sports"], "sports-options");
-createCheckboxes("hobby", category["hobby"], "hobby-options");
-createCheckboxes("journey", category["journey"], "journey-options");
-createCheckboxes("others", category["others"], "others-options");
-*/
